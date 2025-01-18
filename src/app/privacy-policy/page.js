049e8702c8f567ec7d56707b36567c7b67086018
jaimansoni <jaimansoni@gmail.com>
2025-01-18 16:07:28 +0530

    Made Privacy And Policy Pages

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,211 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex flex-col w-full  bg-black text-white ">
+      <div className="flex flex-col items-center  w-full">
+        <div className=" strip w-full">
+        <div className=" flex flex-col gap-y-[20px] items-center justify-center w-[50%] h-screen m-auto max-md:w-[80%]">
+          <h1 className="text-[50px] text-center max-sm:text-[35px]">Privacy Policy</h1>
+          <p className="text-[20px] text-center max-md:text-[15px]">
+            Welcome to Project Astra (“we,” “us,” or “our”). Protecting your
+            personal information is of utmost importance to us. This Privacy
+            Policy explains how we collect, use, and protect the information you
+            share with us through our website. By accessing or using our
+            website, you agree to this Privacy Policy.
+          </p>
+        </div>
+        </div>
+
+        <div className="flex flex-col  gap-y-[60px] bg-[#090909] w-[70%] p-[40px] max-sm:w-[85%] max-sm:p-[20px]">
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+                1. Information We Collect
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p>We collect the following types of personal information:</p>
+              <p>a. Information You Provide to Us</p>
+              <ul className="pl-[50px] list-disc mb-[15px] max-sm:pl-[35px]">
+                <li>
+                  Contact Information: Name, email address, phone number, and
+                  shipping address to fulfill orders and provide updates.
+                </li>
+                <li>
+                  Payment Information: Billing address and payment details
+                  (processed securely by third-party payment providers).
+                </li>
+              </ul>
+
+              <p>b. Automatically Collected Information</p>
+              <p>
+                When you visit our website, we may collect information such as:
+              </p>
+              <ul className="pl-[50px] list-disc max-sm:pl-[35px]">
+                <li>IP address</li>
+                <li>Browser type</li>
+                <li>Device information</li>
+                <li>Website usage data (via cookies and analytics tools)</li>
+              </ul>
+            </div>
+          </section>
+
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+                2. How We Use Your Information
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p>
+                We use your personal information for the following purposes:
+              </p>
+              <ul className="pl-[50px] list-disc max-sm:pl-[35px]">
+                <li>To process and deliver your orders.</li>
+                <li>
+                  To communicate updates, including upcoming drops, exclusive
+                  offers, and promotions.
+                </li>
+                <li>To enhance your shopping experience on our website.</li>
+                <li>To comply with legal obligations or resolve disputes.</li>
+              </ul>
+            </div>
+          </section>
+
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+                3. Sharing of Information
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p>
+                We do not sell or share your personal information with third
+                parties, except:
+              </p>
+              <ul className="pl-[50px] list-disc max-sm:pl-[35px]">
+                <li>
+                  With service providers (e.g., shipping, payment processors)
+                  for the purpose of fulfilling your orders.
+                </li>
+                <li>As required by law or to protect our legal rights.</li>
+              </ul>
+            </div>
+          </section>
+
+
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+              4. Cookies and Tracking Technologies
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p>
+              We use cookies and similar technologies to enhance your experience and analyze website traffic. You can manage your cookie preferences in your browser settings.
+              </p>
+            </div>
+          </section>
+
+
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+              5. Data Security
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p>
+              We implement strict measures to protect your information from unauthorized access, alteration, or disclosure. However, no data transmission over the internet is entirely secure, and we cannot guarantee absolute security.
+              </p>
+            </div>
+          </section>
+
+          
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+              6. Your Rights
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p>
+              You have the right to:
+              </p>
+              <ul className="pl-[50px] list-disc max-sm:pl-[35px]">
+                <li>
+                Access, update, or delete your personal information.</li>
+                <li>Opt out of receiving marketing communications at any time by clicking the “unsubscribe” link in our emails.</li>
+              </ul>
+              <p>For assistance, please contact us at shop@projectastrastore.com.</p>
+            </div>
+          </section>
+
+
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+              7. Retention of Information
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p>
+              We retain your personal information only as long as necessary to fulfill the purposes outlined in this policy, unless a longer retention period is required by law.
+              </p>
+            </div>
+          </section>
+          
+
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+              8. Third-Party Links
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p>
+              Our website may contain links to third-party websites. We are not responsible for their privacy practices or content.
+              </p>
+            </div>
+          </section>
+          
+
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+              9. Policy Updates
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p>
+              We may update this Privacy Policy from time to time. Changes will be effective upon posting on our website.
+              </p>
+            </div>
+          </section>
+
+
+          <section className="flex flex-col gap-y-[20px]">
+            <div>
+              <h1 className="text-[30px] text-center max-sm:text-[25px]">
+              10. Contact Us
+              </h1>
+            </div>
+            <div className="max-sm:text-[15px]">
+              <p className="mb-[15px]">
+              For any questions about this Privacy Policy or your personal information, contact us at:
+              </p>
+              <p>Project Astra Support Team</p>
+              <p>Email: shop@projectastrastore.com</p>
+              <p>Phone: 9904990808 (WhatsApp only)</p>
+            </div>
+          </section>
+          
+          <p>This Privacy Policy is part of our commitment to transparency and customer trust. Thank you for choosing Project Astra.</p>
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
