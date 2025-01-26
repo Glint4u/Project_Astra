82149d3d65d45350031f88c41fa0ee29252f68f2
jaimansoni <jaimansoni@gmail.com>
2025-01-26 10:52:47 +0530

    added checkout

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -15,7 +15,7 @@ export default function ProductCard({ data }) {
                         {data.title}
                     </div>
                     <div className='font-thin'>
-                        $ {data.price}
+                        ₹ {data.price}
                     </div>
 
                 </div>
